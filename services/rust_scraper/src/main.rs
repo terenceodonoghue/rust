baa12fa1dc1960058a0fa8b18e1a5e4f794a3784
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2024-04-03 22:05:14 +1100

    Set up Selenium config (WIP)

--- services/rust_scraper/src/main.rs
+++ services/rust_scraper/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    let server = env::var("SELENIUM_SERVER").unwrap_or("http://localhost:9515".to_string());
+    let target = env::var("SELENIUM_TARGET").expect("TODO:");
+
+    let chrome = DesiredCapabilities::chrome();
+    let driver = WebDriver::new(&server, chrome).await?;
+
+    driver.goto(&target).await?;
+
+    // TODO:
+
+    driver.quit().await?;
+
+    Ok(())
 }
